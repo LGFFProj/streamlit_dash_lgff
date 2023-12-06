6294202743f989cedc4a7cc36b4ff299c2907fa1
LuizGFF_Office <luizferreiraprojetos@gmail.com>
2023-12-06 08:48:37 -0300

    Dataset pelo Github

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 
-dt = pd.read_csv("./dataset/BTCUSDT-15m.csv")
+dt = pd.read_csv("https://raw.githubusercontent.com/LGFFProj/streamlit_dash_lgff/main/dataset/BTCUSDT-15m.csv")
 
 
 st.table(dt)
